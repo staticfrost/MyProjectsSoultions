a9ff3b5e1ff15ab87a1a501677c268a4b89be772
Scott <static@staticfrost.com>
2016-10-03 14:29:36 +1000

    Wage calculator
    Calculates what your weekly net pay will be.

---
+++ Wages/Wages/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Wages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.txtbName = new System.Windows.Forms.TextBox();
+            this.txtbHourlyRate = new System.Windows.Forms.TextBox();
+            this.txtbHoursWorked = new System.Windows.Forms.TextBox();
+            this.btnCalulate = new System.Windows.Forms.Button();
+            this.lblGrossPay = new System.Windows.Forms.Label();
+            this.lblTaxes = new System.Windows.Forms.Label();
+            this.lblNetPay = new System.Windows.Forms.Label();
+            this.txtbGrossPay = new System.Windows.Forms.TextBox();
+            this.txtbTaxes = new System.Windows.Forms.TextBox();
+            this.txtbNetPay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(7, 7);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(125, 13);
+            this.lblDesc.TabIndex = 0;
+            this.lblDesc.Text = "Please enter your details:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(11, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(12, 58);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(66, 13);
+            this.lblHourlyRate.TabIndex = 2;
+            this.lblHourlyRate.Text = "Hourly Rate:";
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(13, 83);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(79, 13);
+            this.lblHoursWorked.TabIndex = 3;
+            this.lblHoursWorked.Text = "Hours Worked:";
+            // 
+            // txtbName
+            // 
+            this.txtbName.Location = new System.Drawing.Point(79, 33);
+            this.txtbName.Name = "txtbName";
+            this.txtbName.Size = new System.Drawing.Size(135, 20);
+            this.txtbName.TabIndex = 4;
+            // 
+            // txtbHourlyRate
+            // 
+            this.txtbHourlyRate.Location = new System.Drawing.Point(154, 58);
+            this.txtbHourlyRate.Name = "txtbHourlyRate";
+            this.txtbHourlyRate.Size = new System.Drawing.Size(59, 20);
+            this.txtbHourlyRate.TabIndex = 5;
+            // 
+            // txtbHoursWorked
+            // 
+            this.txtbHoursWorked.Location = new System.Drawing.Point(154, 85);
+            this.txtbHoursWorked.Name = "txtbHoursWorked";
+            this.txtbHoursWorked.Size = new System.Drawing.Size(59, 20);
+            this.txtbHoursWorked.TabIndex = 6;
+            // 
+            // btnCalulate
+            // 
+            this.btnCalulate.Location = new System.Drawing.Point(27, 124);
+            this.btnCalulate.Name = "btnCalulate";
+            this.btnCalulate.Size = new System.Drawing.Size(238, 32);
+            this.btnCalulate.TabIndex = 7;
+            this.btnCalulate.Text = "Calculate";
+            this.btnCalulate.UseVisualStyleBackColor = true;
+            this.btnCalulate.Click += new System.EventHandler(this.btnCalulate_Click);
+            // 
+            // lblGrossPay
+            // 
+            this.lblGrossPay.AutoSize = true;
+            this.lblGrossPay.Location = new System.Drawing.Point(25, 186);
+            this.lblGrossPay.Name = "lblGrossPay";
+            this.lblGrossPay.Size = new System.Drawing.Size(58, 13);
+            this.lblGrossPay.TabIndex = 8;
+            this.lblGrossPay.Text = "Gross Pay:";
+            // 
+            // lblTaxes
+            // 
+            this.lblTaxes.AutoSize = true;
+            this.lblTaxes.Location = new System.Drawing.Point(24, 209);
+            this.lblTaxes.Name = "lblTaxes";
+            this.lblTaxes.Size = new System.Drawing.Size(78, 13);
+            this.lblTaxes.TabIndex = 9;
+            this.lblTaxes.Text = "How much tax:";
+            // 
+            // lblNetPay
+            // 
+            this.lblNetPay.AutoSize = true;
+            this.lblNetPay.Location = new System.Drawing.Point(26, 231);
+            this.lblNetPay.Name = "lblNetPay";
+            this.lblNetPay.Size = new System.Drawing.Size(48, 13);
+            this.lblNetPay.TabIndex = 10;
+            this.lblNetPay.Text = "Net Pay:";
+            // 
+            // txtbGrossPay
+            // 
+            this.txtbGrossPay.Location = new System.Drawing.Point(122, 179);
+            this.txtbGrossPay.Name = "txtbGrossPay";
+            this.txtbGrossPay.Size = new System.Drawing.Size(92, 20);
+            this.txtbGrossPay.TabIndex = 11;
+            // 
+            // txtbTaxes
+            // 
+            this.txtbTaxes.Location = new System.Drawing.Point(122, 204);
+            this.txtbTaxes.Name = "txtbTaxes";
+            this.txtbTaxes.Size = new System.Drawing.Size(92, 20);
+            this.txtbTaxes.TabIndex = 12;
+            // 
+            // txtbNetPay
+            // 
+            this.txtbNetPay.Location = new System.Drawing.Point(122, 228);
+            this.txtbNetPay.Name = "txtbNetPay";
+            this.txtbNetPay.Size = new System.Drawing.Size(92, 20);
+            this.txtbNetPay.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 278);
+            this.Controls.Add(this.txtbNetPay);
+            this.Controls.Add(this.txtbTaxes);
+            this.Controls.Add(this.txtbGrossPay);
+            this.Controls.Add(this.lblNetPay);
+            this.Controls.Add(this.lblTaxes);
+            this.Controls.Add(this.lblGrossPay);
+            this.Controls.Add(this.btnCalulate);
+            this.Controls.Add(this.txtbHoursWorked);
+            this.Controls.Add(this.txtbHourlyRate);
+            this.Controls.Add(this.txtbName);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblDesc);
+            this.Name = "Form1";
+            this.Text = "Wage Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.TextBox txtbName;
+        private System.Windows.Forms.TextBox txtbHourlyRate;
+        private System.Windows.Forms.TextBox txtbHoursWorked;
+        private System.Windows.Forms.Button btnCalulate;
+        private System.Windows.Forms.Label lblGrossPay;
+        private System.Windows.Forms.Label lblTaxes;
+        private System.Windows.Forms.Label lblNetPay;
+        private System.Windows.Forms.TextBox txtbGrossPay;
+        private System.Windows.Forms.TextBox txtbTaxes;
+        private System.Windows.Forms.TextBox txtbNetPay;
+    }
+}
+
---
+++ Wages/Wages/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalulate_Click(object sender, EventArgs e)
+        {
+            /*Write a program which accepts an employees name, rate of pay and hours worked. The
+            following values should then be calculated: Gross pay, Taxes (20 % of gross pay), and net
+            pay.*/
+            string name;
+            double hourlyRate;
+            int hoursWorked;
+
+            double grossPay;
+            double taxes;
+            double netPay;
+
+            name = txtbName.Text;
+            hourlyRate = double.Parse(txtbHourlyRate.Text); //convert string to double from the text box
+            hoursWorked = int.Parse(txtbHoursWorked.Text); //convert string to int from the text box
+            grossPay = hourlyRate * hoursWorked; // Gross pay is hourly rate * hours worked.
+            txtbGrossPay.Text = (grossPay.ToString()); // print gross pay to text box
+
+            //taxes are 20% of the gross pay.
+            taxes = grossPay * 0.20;
+            txtbTaxes.Text = (taxes.ToString());
+
+            //net pay is gross pay - taxes
+            netPay = grossPay - taxes;
+            txtbNetPay.Text = (netPay.ToString());
+
+
+        }
+    }
+}
