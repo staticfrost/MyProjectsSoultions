e3f034a8d085cb29936f71144d1bb3e34fa72f06
Scott <static@staticfrost.com>
2016-10-09 18:34:49 +1000

    Ticket Price Calculator
    Want to know how much tickets should be changed for an age group. Basic Windows Form Application to do this.

---
+++ tickets/tickets/FrmTickets.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace tickets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTickets = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtBoxAge = new System.Windows.Forms.TextBox();
+            this.txtBoxPrice = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnTickets
+            // 
+            this.BtnTickets.Location = new System.Drawing.Point(73, 144);
+            this.BtnTickets.Name = "BtnTickets";
+            this.BtnTickets.Size = new System.Drawing.Size(148, 30);
+            this.BtnTickets.TabIndex = 0;
+            this.BtnTickets.Text = "Determine Ticket Price";
+            this.BtnTickets.UseVisualStyleBackColor = true;
+            this.BtnTickets.Click += new System.EventHandler(this.BtnTickets_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Age:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ticket Price";
+            // 
+            // TxtBoxAge
+            // 
+            this.TxtBoxAge.Location = new System.Drawing.Point(135, 50);
+            this.TxtBoxAge.Name = "TxtBoxAge";
+            this.TxtBoxAge.Size = new System.Drawing.Size(39, 20);
+            this.TxtBoxAge.TabIndex = 3;
+            // 
+            // txtBoxPrice
+            // 
+            this.txtBoxPrice.Location = new System.Drawing.Point(169, 76);
+            this.txtBoxPrice.Name = "txtBoxPrice";
+            this.txtBoxPrice.Size = new System.Drawing.Size(70, 20);
+            this.txtBoxPrice.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtBoxPrice);
+            this.Controls.Add(this.TxtBoxAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTickets);
+            this.Name = "Form1";
+            this.Text = "Tickets";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTickets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtBoxAge;
+        private System.Windows.Forms.TextBox txtBoxPrice;
+    }
+}
+
---
+++ tickets/tickets/FrmTickets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tickets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTickets_Click(object sender, EventArgs e)
+        {
+            
+            int age = 0;
+            double price = 0;
+            age = int.Parse(TxtBoxAge.Text);
+            
+
+            if (age <=12)
+            {
+                price = 5.00;
+            }
+            else if (age >=13 && age <=20)
+            {
+                price = 6.00;
+            }
+            else if (age >=21 && age <=64)
+            {
+                price = 10.00;
+            }
+            else if (age >= 65)
+            {
+                price = 2.00;
+
+            }
+            txtBoxPrice.Text = (price.ToString());
+
+        }
+    }
+}
