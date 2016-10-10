b20dbfeb7ff53944f0d49964450573a008596f68
Scott <static@staticfrost.com>
2016-10-10 20:34:44 +1000

    Determine if you've passed or failed
    C# windows form application that will work out if you've passed or failed.

---
+++ avggrade/avggrade/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace avggrade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.txtBoxAverage = new System.Windows.Forms.TextBox();
+            this.lblPorF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter your four grades";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Average mark:";
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(17, 132);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(85, 22);
+            this.BtnCalculate.TabIndex = 2;
+            this.BtnCalculate.Text = "Calculate Average";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(19, 57);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(38, 20);
+            this.txtBox1.TabIndex = 3;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(63, 57);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(39, 20);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(109, 57);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(45, 20);
+            this.txtBox3.TabIndex = 5;
+            // 
+            // txtBox4
+            // 
+            this.txtBox4.Location = new System.Drawing.Point(160, 57);
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.Size = new System.Drawing.Size(38, 20);
+            this.txtBox4.TabIndex = 6;
+            // 
+            // txtBoxAverage
+            // 
+            this.txtBoxAverage.Location = new System.Drawing.Point(109, 160);
+            this.txtBoxAverage.Name = "txtBoxAverage";
+            this.txtBoxAverage.Size = new System.Drawing.Size(41, 20);
+            this.txtBoxAverage.TabIndex = 7;
+            // 
+            // lblPorF
+            // 
+            this.lblPorF.AutoSize = true;
+            this.lblPorF.Location = new System.Drawing.Point(20, 196);
+            this.lblPorF.Name = "lblPorF";
+            this.lblPorF.Size = new System.Drawing.Size(0, 13);
+            this.lblPorF.TabIndex = 8;
+            this.lblPorF.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblPorF);
+            this.Controls.Add(this.txtBoxAverage);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Average Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.TextBox txtBoxAverage;
+        private System.Windows.Forms.Label lblPorF;
+    }
+}
+
