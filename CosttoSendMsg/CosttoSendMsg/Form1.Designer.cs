5929c0e01b9d721baf26660ebd6304bb78db01a3
Scott <static@staticfrost.com>
2016-10-13 21:51:07 +1000

    Cost to send a telegram
    Assume that the cost, not including tax, for sending a telegram from Hamilton to Montreal is $4.50 for the first twenty
     words or less, plus $.17 for each additional word beyond twenty. Write a program which accepts the number of words
    and then determines the total cost.

---
+++ CosttoSendMsg/CosttoSendMsg/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CosttoSendMsg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxWords = new System.Windows.Forms.TextBox();
+            this.txtboxPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter how many words you\'d like to send";
+            // 
+            // txtboxWords
+            // 
+            this.txtboxWords.Location = new System.Drawing.Point(111, 100);
+            this.txtboxWords.Name = "txtboxWords";
+            this.txtboxWords.Size = new System.Drawing.Size(41, 20);
+            this.txtboxWords.TabIndex = 2;
+            // 
+            // txtboxPrice
+            // 
+            this.txtboxPrice.Location = new System.Drawing.Point(111, 177);
+            this.txtboxPrice.Name = "txtboxPrice";
+            this.txtboxPrice.Size = new System.Drawing.Size(41, 20);
+            this.txtboxPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The price will be:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxPrice);
+            this.Controls.Add(this.txtboxWords);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Telegram Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxWords;
+        private System.Windows.Forms.TextBox txtboxPrice;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CosttoSendMsg/CosttoSendMsg/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CosttoSendMsg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* Assume that the cost, not including tax, for sending a telegram from Hamilton to Montreal is $4.50 for the first twenty
+            words or less, plus $.17 for each additional word beyond twenty. Write a program which accepts the number of words
+            and then determines the total cost. */
+            double telegram = 4.5;
+            double extra = 0.17;
+
+            int words;
+            int word2;
+            double cost;
+            words = int.Parse(txtboxWords.Text);
+
+            if (words >= 20)
+            {
+                word2 = words - 20;
+                cost = telegram + (word2 * extra);
+                
+                txtboxPrice.Text = (cost.ToString());
+
+            }
+            else if (words <= 20)
+            {
+                cost = telegram;
+                txtboxPrice.Text = (cost.ToString());
+               
+            }
+
+        }
+    }
+}
