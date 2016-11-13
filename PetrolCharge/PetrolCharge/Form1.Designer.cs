df7bf4ec003b6d758368da8d43b452e78ea09cbb
Scott <static@staticfrost.com>
2016-11-13 20:13:09 +1000

    Petrol Charge rate per KM
    Application to work out how much a car ride will cost based on distance traveled. 
    
    (Doesn't have real world values, but wouldn't be hard to work that bit out)

---
+++ PetrolCharge/PetrolCharge/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PetrolCharge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtboxDistance = new System.Windows.Forms.TextBox();
+            this.txtboxCost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bntCalculate
+            // 
+            this.bntCalculate.Location = new System.Drawing.Point(95, 166);
+            this.bntCalculate.Name = "bntCalculate";
+            this.bntCalculate.Size = new System.Drawing.Size(75, 23);
+            this.bntCalculate.TabIndex = 0;
+            this.bntCalculate.Text = "Calculate";
+            this.bntCalculate.UseVisualStyleBackColor = true;
+            this.bntCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter distance travelled";
+            // 
+            // txtboxDistance
+            // 
+            this.txtboxDistance.Location = new System.Drawing.Point(176, 69);
+            this.txtboxDistance.Name = "txtboxDistance";
+            this.txtboxDistance.Size = new System.Drawing.Size(50, 20);
+            this.txtboxDistance.TabIndex = 2;
+            // 
+            // txtboxCost
+            // 
+            this.txtboxCost.Location = new System.Drawing.Point(94, 138);
+            this.txtboxCost.Name = "txtboxCost";
+            this.txtboxCost.Size = new System.Drawing.Size(75, 20);
+            this.txtboxCost.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Price:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxCost);
+            this.Controls.Add(this.txtboxDistance);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bntCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxDistance;
+        private System.Windows.Forms.TextBox txtboxCost;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PetrolCharge/PetrolCharge/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetrolCharge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /* A gas company measures the amount of gas its customers use in hundreds of cubic feet (ccf) and charges them
+            according to the following schedule:
+            first 8 ccf 50 cents each //change this to 5KM
+            more than 8 ccf 30 cents each // 5KM
+            Note that the 30 cent rate applies only to usage above 8 ccf. A customer who uses 10 ccf is charged 50 cents for the
+            first 8 ccf and 30 cents for the next 2 ccf. Write a program to calculate customer’s bills.*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int distance;
+            distance = int.Parse(txtboxDistance.Text);
+            double price = 0;
+                        
+            if (distance <= 8)
+            {
+                price = distance * 0.50;
+            }
+            else if (distance > 8)
+            {
+                price = 8 * 0.50 + (distance-8) * .30; 
+            }
+                        
+            price = double.Parse(price.ToString());
+            txtboxCost.Text = price.ToString();
+        }
+    }
+}
